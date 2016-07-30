413c29289fc1dc4544b42dd9e4826841f1152a9e
Vladimir Trifonov <trifonov.dev@gmail.com>
2016-07-30 18:03:09 +0300

    Installed jspm and twitter bootstrap

---
+++ web/public/build.js
@@ -0,0 +1,2 @@
+System.register("app.js",[],function(a,b){"use strict";b&&b.id;return{setters:[],execute:function(){"bootstrap/css/bootstrap.css!"}}});
+//# sourceMappingURL=build.js.map
---
+++ web/public/config.js
@@ -0,0 +1,27 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "bootstrap": "github:twbs/bootstrap@3.3.7",
+    "css": "github:systemjs/plugin-css@0.1.25",
+    "typescript": "npm:typescript@1.8.10",
+    "github:jspm/nodelibs-os@0.1.0": {
+      "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "github:twbs/bootstrap@3.3.7": {
+      "jquery": "npm:jquery@2.2.4"
+    },
+    "npm:os-browserify@0.1.2": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:typescript@1.8.10": {
+      "os": "github:jspm/nodelibs-os@0.1.0"
+    }
+  }
+});
