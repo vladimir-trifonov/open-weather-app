5fd133b0240d6e96a305d372c741a4355e97a38c
Vladimir Trifonov <trifonov.dev@gmail.com>
2016-07-31 22:50:16 +0300

    front - location service refactoring

--- web/public/app/common/utils/location.ts
+++ web/public/app/common/utils/location.ts
@@ -1,9 +1,26 @@
 export default {
 	getLocation: (cb) => {
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(cb);
+			new Promise((resolve, reject) => {
+				let t = window.setTimeout(() => {
+					reject(null)
+				}, 8000);
+
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						window.clearTimeout(t);
+						resolve(position);
+					},
+					(error) => {
+						window.clearTimeout(t);
+						reject(error);
+					}
+				);
+			}).then(cb).catch(() => {
+				cb(null);
+			});
 		} else {
-			cb(null);
+			cb(null)
 		}
 	}
 }
