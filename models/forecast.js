2b0d75cf3df84939ef915444dc7085b1aeb0a1c5
Vladimir Trifonov <trifonov.dev@gmail.com>
2016-07-31 11:22:06 +0300

    connector's test model changed

--- models/forecast.js
+++ models/forecast.js
@@ -3,35 +3,10 @@ var Arrow = require('arrow');
 var Forecast = Arrow.Model.extend('forecast', {
 	fields: {
 		"city": {
-			"id": {
-				type: Number
-			},
-			"name": {
-				type: String
-			},
-			"coord": {
-				"lon": {
-					type: Number
-				},
-				"lat": {
-					type: Number
-				}
-			},
-			"country": {
-				type: String
-			},
-			"cod": {
-				type: String
-			},
-			"message": {
-				type: Number
-			},
-			"cnt": {
-				type: Number
-			},
-			"list": {
-				type: Array
-			}
+			type: Object
+		},
+		"list": {
+			type: Array
 		}
 	},
 	connector: 'com.connector.openweathermap'
