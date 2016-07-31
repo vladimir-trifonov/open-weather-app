9247012dceb7965b8deb3b2ad3c1a8f83ec67233
Vladimir Trifonov <trifonov.dev@gmail.com>
2016-07-31 11:32:31 +0300

    connector's configs changed

--- conf/com.connector.openweathermap.default.js
+++ conf/com.connector.openweathermap.default.js
@@ -5,10 +5,10 @@
 module.exports = {
 	connectors: {
 		'com.connector.openweathermap': {
-			appid: 'OPEN_WEATHER_MAP_API_ID',
+			appid: process.env.OPEN_WEATHER_MAP_API_ID,
 			city: 'London',
 			urls: {
-				forecast: 'api.openweathermap.org/data/2.5/forecast'
+				forecast: 'http://api.openweathermap.org/data/2.5/forecast'
 			}
 		}
 	}
